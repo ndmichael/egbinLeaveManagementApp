3adc3454016a6fc8e16c156164bf24c4cceccf16
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-04-04 15:59:59 +0100

    test

--- main/settings.py
+++ main/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'ip_(*mm*wiydqtss3we2d48&a@!$net1#&@8kw7w1+wo#-5uhr'
+SECRET_KEY = 'ip_(*mm*wiydqtss3we2d48&a@!$net1#&@8kw7w1+wo#-5uhr'
 
-SECRET_KEY = os.environ.get('SECRET_KEY_EGBINAPP')
+# SECRET_KEY = os.environ.get('SECRET_KEY_EGBINAPP')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
