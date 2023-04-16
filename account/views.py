8f5ef7201cf18297ae0dc0d3462a35307aede511
ndmichael <ukejemicheal@gmail.com>
2023-04-17 00:11:45 +0100

    added a redirect on success on the login custom view

--- account/views.py
+++ account/views.py
@@ -3,14 +3,22 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from account.models import Staff
 from leaveapp.models import Leave
-
+from django.contrib.auth.decorators import login_required
 from .forms import CustomAuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
 
 
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
 
+    def get_success_url(self):
+        url = self.get_redirect_url()
+        return url or reverse('staff-detail', kwargs={'id': self.request.user.pk, 'username': self.request.user.username})
 
+
+
+@login_required
 def staff_detail (request, id, username):
     staff = Staff.objects.get(user=id)
     leave_requested = Leave.objects.filter(user=id).count()
