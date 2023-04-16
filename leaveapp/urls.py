75b78de1cc80eafa382ce1ddb748078e04eb5b58
ndmichael <ukejemicheal@gmail.com>
2023-04-17 00:10:11 +0100

    commented out indexpage url

--- leaveapp/urls.py
+++ leaveapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from leaveapp import views as leave_views
 
 urlpatterns = [
-    path('', leave_views.index, name="app-home" ),
+    # path('', leave_views.index, name="app-home" ),
     path('request/', leave_views.RequestLeave, name="requestleave" ),
     path('history/', leave_views.historyList, name="history" ),
     path('howtouse/', leave_views.howToUse, name="how_to_use" ),
