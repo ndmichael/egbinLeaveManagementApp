28fcd4049a5681237ad812c20c85d06fc7626a7b
ndmichael <ukejemicheal@gmail.com>
2023-04-16 17:12:11 +0100

    added allowed host

--- main/settings.py
+++ main/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ukejegbinapp.herokuapp.com']
+ALLOWED_HOSTS = ['*', 'egbinleave.xyz', 'ukejegbinapp.herokuapp.com']
 
 
 # Application definition
